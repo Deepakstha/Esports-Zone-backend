16cdad8e5923a297df0e86fb0646cd50e5fd65cb
Deepakstha <deepakstha79@gmail.com>
2023-11-24 22:19:56 +0545

    getting user wallet

--- controller/wallet/walletController.js
+++ controller/wallet/walletController.js
@@ -10,3 +10,27 @@ exports.createWallet = async (userId) => {
     return { success: false, error };
   }
 };
+
+exports.getUserWallet = async (req, res, next) => {
+  const userId = req.user.id;
+
+  try {
+    const userWallet = await Wallet.findOne({
+      where: {
+        userId: userId,
+      },
+    });
+    if (userWallet) {
+      res.status(200).json({
+        message: "Wallet found !",
+        data: userWallet,
+      });
+    } else {
+      res.status(400).json({
+        message: "Wallet of the user not found !",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
