ed14af8a081b7f5aa7b8e451d923e2a60b20c5c1
Deepakstha <deepakstha79@gmail.com>
2023-11-24 22:33:10 +0545

    update delete and display prize pool

--- controller/prizePool/prizePoolController.js
+++ controller/prizePool/prizePoolController.js
@@ -90,3 +90,85 @@ exports.getSinglePrizePool = async (req, res) => {
     });
   }
 };
+
+// Update Prize Pool
+exports.updatePrizePool = async (req, res) => {
+  const { prize, placements } = req.body;
+  const prizePoolId = req.params.id;
+
+  console.log(prize, "prize");
+  console.log(req.body, "body");
+  console.log(prizePoolId, "Pid");
+
+  if (!prize) {
+    return res.status(400).json({
+      message: "Prize is empty !",
+      status: 400,
+    });
+  }
+
+  try {
+    const prizePool = await PrizePool.findByPk(prizePoolId);
+
+    if (prize) {
+      prizePool.prize = prize;
+    }
+    prizePool.save();
+
+    return res.status(200).json({
+      message: "Prize pool updated successfully",
+      data: prizePool,
+      status: 200,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+      status: 500,
+    });
+  }
+};
+
+// delete prizepool
+exports.deletePrizePool = async (req, res) => {
+  const prizePoolId = req.params.id;
+  console.log(prizePoolId, "prizePoolid");
+  // return;
+
+  try {
+    const prizePool = await PrizePool.destroy({
+      where: {
+        id: prizePoolId,
+      },
+    });
+    if (prizePool != 0) {
+      return res.status(200).json({
+        message: "Prize pool deleted successfully",
+        status: 200,
+      });
+    } else {
+      return res.status(400).json({
+        message: "Prize pool doesn't exist or deleted already.",
+        status: 400,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      status: 500,
+    });
+  }
+};
+
+exports.displayPrizePoolByTournamentId = async (req, res, next) => {
+  const { tournamentId } = req.params;
+  const prizePoolWithTournament = await PrizePool.findAll({
+    where: { tournamentId },
+  });
+
+  const sortPrizePool = prizePoolWithTournament.sort(
+    (a, b) => a.placements - b.placements
+  );
+
+  // console.log(JSON.stringify(sortPrizePool));
+  return res.status(200).json(sortPrizePool);
+};
