9127a68e9d1dec474e9c469f491ad87f22e434cc
Deepakstha <deepakstha79@gmail.com>
2023-11-24 23:01:14 +0545

    display teams by id

--- controller/team/teamController.js
+++ controller/team/teamController.js
@@ -67,3 +67,29 @@ exports.createTeams = async (req, res, next) => {
     next(error);
   }
 };
+
+//display teams by user id (leader)
+exports.displayTeamsByUserId = async (req, res, next) => {
+  const userId = req.user.id;
+  const teamsByUser = await Teams.findAll({ where: { userId } });
+  return res.json({ teams: teamsByUser });
+};
+
+// get teams by id
+exports.getById = async (req, res, next) => {
+  const teamId = req.params.id;
+
+  try {
+    const teams = await Teams.findOne({
+      where: { id: teamId },
+      include: { model: db.user, attributes: ["username", "fullname"] },
+    });
+
+    if (!teams) {
+      return res.status(404).json({ message: "Teams Not Found", status: 404 });
+    }
+    return res.status(200).json({ teams, status: 200 });
+  } catch (error) {
+    next(error);
+  }
+};
